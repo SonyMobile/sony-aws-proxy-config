3f642389e82b392b9d0ae69420dde9e5afd86a8a
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-02-04 11:26:11 +0100

    Implemented sonyAwsProxyConfig

--- test/sony-aws-proxy-config-test.js
+++ test/sony-aws-proxy-config-test.js
@@ -5,7 +5,34 @@ var expect = require('chai').expect;
 var sonyAwsProxyConfig = require('../lib/sony-aws-proxy-config');
 
 describe('#sonyAwsProxyConfig', function() {
-    it('responds with hello', function() {
-        expect(sonyAwsProxyConfig()).to.equal('hello');
+
+    describe('with no http_proxy', function() {
+        it('responds with {}', function() {
+            delete process.env.http_proxy;
+            expect(sonyAwsProxyConfig()).to.deep.equal({});
+        });
+    });
+
+    describe('with sony global proxy', function() {
+        it('responds with direct proxy', function() {
+            process.env.http_proxy =
+                'http://proxy.global.sonyericsson.net:8080';
+            expect(sonyAwsProxyConfig()).to.deep.equal({
+                httpOptions: {
+                    proxy: "http://proxy.global.sonyericsson.net:8080"
+                }
+            });
+        });
+    });
+    describe('with cntlm proxy', function() {
+        it('responds with agent proxy', function() {
+            process.env.http_proxy = 'http://localhost:3128/'
+            var config = sonyAwsProxyConfig();
+            expect(config.httpOptions.agent).to.be.defined;
+            expect(config.httpOptions.agent.proxyOptions).to.deep.equal({
+                host: 'localhost',
+                port: 3128
+            });
+        });
     });
 });
